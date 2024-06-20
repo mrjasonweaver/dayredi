d0d7a5aa25852b1d2e5dd34d9888188ac019371c
Jason Weaver <indyplanets@gmail.com>
2024-06-20 08:37:39 -0500

    add unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
   build: {
     sourcemap: true,
-},
+  },
 })
